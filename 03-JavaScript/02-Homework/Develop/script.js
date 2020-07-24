2b4575a61a1445aa9636508af6f4d75dfc8c53c6
LLHanuman <66429121+LLHanuman@users.noreply.github.com>
2020-07-24 13:42:09 -0500

    Add files via upload

--- 03-JavaScript/02-Homework/Develop/script.js
+++ 03-JavaScript/02-Homework/Develop/script.js
@@ -1,88 +1,91 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var generatedPassword = ""
 
 // Write password to the #password input
 
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-
-function passwordLength() {
-  const span = prompt("How long would you like your new password to be? Please choose greater than 8 and less than 128.") 
-    if(span < 8) {
-    alert('Please enter a number above 8.')}
-    if(span > 128) {
-    alert('Please enter a number below 128.')}
-}
-passwordLength()
 
 
-var letter = "";
-var uppercase = "abcdefghijklmnopqrstuvwxyz";
-
-while (letter.length < 6) {
-  letter += uppercase[Math.floor(Math.random() * uppercase.length)];
-} 
+var symbol = '!@#$%^&*()'
+var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var number = '0123456789'
 
 
-function getRandomUpper() {
-  var uppercase = confirm ("Would you like uppercase letters in your password?")
-   if (uppercase == true) {
-    console.log(letter);
-   }
- }
- getRandomUpper()
-
- var letter = "";
- var uppercase = "abcdefghijklmnopqrstuvwxyz";
- 
- while (letter.length < 6) {
-   letter += uppercase[Math.floor(Math.random() * uppercase.length)];
- } 
-
-function getRandomLower() {
-  var lowercase = confirm ("would you like lowercase letters in your password?")
-  if (lowercase == true) { 
-    console.log(letter);
-}}
-  getRandomLower()
-
-
-function getRandomNumber() {
-var number = confirm ("Would you like numbers in your password?")
-  if (number == true) {
-    for (var i = 0; i < 6; i++) {
-      var num = Math.floor(Math.random() * 10) +1;
-    console.log(num + i)
+function completePassword() {
+  var askLength = prompt ('How long would you like your password to be?')
+    if (askLength < 8) {
+      alert('Please choose a number higher than 8.')
     }
-  }}
-  getRandomNumber()
+    if (askLength > 128) {
+      alert('Please choose a number lower than 128.')
+    }
+  passLength = askLength;
+
+  var askAlphabet = confirm ('Would you like Uppercase letters in your password?')
+  if (askAlphabet == true) {
+    addAlpha = alphabet
+  }
+  if (askAlphabet == false) {
+    addAlpha = ''
+  }
+
+  var askNumber = confirm('Would you like numbers in your password?')
+  if (askNumber == true){
+    passWNum = number
+  }
+  if (askNumber == false){
+    passWNum = ''
+  }
+
+  var askSymbol = confirm('Would you like symbols in your password?')
+    if (askSymbol == true) {
+      passWSym = symbol
+  }  
+    if (askSymbol == false) {
+      passWSym = ''
+  }
 
 
-var empty = "";
-var symbol = "!@#$%&*";
+String.prototype.shuffle = function () {
+  var a = this.split(""),
+      n = a.length;
 
-while (empty.length < 5) {
-  empty += symbol[Math.floor(Math.random() * symbol.length)];
-} 
-
-
-function getRandomSymbol() {
-  var symbol = confirm ("Would you like symbols in your password??")
-   if (symbol == true) {
-    console.log(empty);
-   }
- }
- getRandomSymbol()
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-function genPassword(getRandomLower, getRandomNumber, getRandomSymbol, getRandomUpper) {
-
+  for(var i = n - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var tmp = a[i];
+      a[i] = a[j];
+      a[j] = tmp;
+  }
+  return a.join("");
 }
-document.getElementById(password)('5+6')
 
+var sumAll = (addAlpha + passWSym + passWNum).shuffle();
+alert('Your new password is: ' + sumAll)
+}
+completePassword()
+
+
+
+// if (completePassword() == '') {
+//   alert('You need to select something.')
+// }
+// alert('Your new password is')
+
+  
+
+// whats the lenght of password
+// save as length variable
+// save a password variable
+// for loop that loops length number of times and do some functionality
+
+//  var number = confirm ("Would you like numbers in your password?")
+//  var letter = confirm ("Would you like numbers in your password?")
+//  var symbol = confirm ("Would you like numbers in your password?")
+
+// for(var i=0; i < length; i++) {
+
+// based on options generate password and add to password global variable
+
+//}
+
+// scramble after 
